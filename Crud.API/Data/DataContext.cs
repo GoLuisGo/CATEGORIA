978e48d16c18a868c3841b3ce86607e6b3c1f170
LUIS FERNANDO ARANGO GOMEZ <luisgomezarango@gmail.com>
2023-03-23 13:06:13 -0500

    actualizacion

---
+++ Crud.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Crud.Shared.Entities;
+
+namespace Stores.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
